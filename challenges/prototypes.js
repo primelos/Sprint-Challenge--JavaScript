aa1e65ea312cbc244bb1d870f5adb674dd8018d6
Ale <alearanibar@gmail.com>
2019-08-23 08:10:49 -0700

    prototype

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -31,3 +31,22 @@
 // console.log(cuboid.surfaceArea()); // 130
 
 
+function CuboidMaker (attributes){
+    this.length = attributes.length;
+    this.width  = attributes.width;
+    this.height = attributes.height;
+}
+CuboidMaker.prototype.volume = function(){
+    return this.length * this.width * this.height
+}
+CuboidMaker.prototype.surfaceArea = function(){
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+}
+
+let cuboid = new CuboidMaker({
+    length: 4, 
+    width: 5, 
+    height: 5,
+})
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
